3b8ea2a340b7fd54fb70756393a778d0dbd1cb15
Prasanth7S <46496589+Prasanth7S@users.noreply.github.com>
2024-03-13 12:38:14 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,21 @@
 output "role_arn" {
-  value = aws_iam_role.bharath_role.arn
+  value = aws_iam_role.prasanth_role.arn
 }
 
 output "role_name" {
-  value = aws_iam_role.bharath_role.name
+  value = aws_iam_role.prasanth_role.name
 }
 
 output "policy_arn" {
-  value = aws_iam_policy.bharath_policy.arn
+  value = aws_iam_policy.prasanth_policy.arn
 }
 
 output "policy_name" {
-  value = aws_iam_policy.bharath_policy.name
+  value = aws_iam_policy.prasanth_policy.name
 }
 
 output "bucket_name" {
-  value = aws_s3_bucket.bharath_bucket.id
+  value = aws_s3_bucket.prasanth_bucket.id
 }
 
 

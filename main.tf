13ab8ad8228192ab2ebaa30ab0cbfdd93de5e6e6
VISHALMACOM <vishalmacom3@gmail.com>
2024-04-16 13:28:08 -0400

    adding ansible code

--- main.tf
+++ main.tf
@@ -1,72 +1,27 @@
-resource "aws_iam_role" "prasanth_role" {
-  name = "prasanth-role"
-  assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [{
-      "Effect" : "Allow",
-      "Principal" : {
-        "Service" : "ec2.amazonaws.com"
-      },
-      "Action" : "sts:AssumeRole"
-    }]
-  })
+module "resource_group" {
+  source     = "/home/vishal/automation/terraform/lab06/modules/resource_group"
+  network-rg = "network-rg"
+  linux-rg   = "linux-rg"
+  windows-rg = "windows-rg"
+  location   = "canadacentral"
 }
 
-resource "aws_iam_policy" "prasanth_policy" {
-  name        = "prasanth-policy"
-  description = "Inline policy for the IAM role"
-
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [{
-      "Effect" : "Allow",
-      "Action" : ["s3:ListBucket"],
-      "Resource" : ["arn:aws:s3:::prasanth-bucket"]
-    }]
-  })
+module "network" {
+  source              = "/home/vishal/automation/terraform/lab07s1/modules/network"
+  network_rg_location = module.resource_group.network_rg_location
+  network_rg_name     = module.resource_group.network_rg_name
 }
 
-resource "aws_iam_role_policy_attachment" "policy-role_attachment" {
-  role       = aws_iam_role.prasanth_role.name
-  policy_arn = aws_iam_policy.prasanth_policy.arn
+module "linux" {
+  source            = "/home/vishal/automation/terraform/lab07s1/modules/linux"
+  linux_rg_location = module.resource_group.linux_rg_location
+  linux_rg_name     = module.resource_group.linux_rg_name
+  subnet1_id        = module.network.subnet1_id
 }
 
-# Use the IAM role to create an S3 bucket
-resource "aws_s3_bucket" "prasanth_bucket" {
-  bucket = "prasanthnew-bucket"
-
-  tags = {
-    Name = "Cloud security sample Bucket"
-  }
-
-}
-
-resource "aws_iam_role_policy_attachment" "remove_policy-role_attachment" {
-  role       = aws_iam_role.prasanth_role.name
-  policy_arn = aws_iam_policy.prasanth_policy.arn
-  count      = 0
-}
-
-resource "null_resource" "detach_policies" {
-  triggers = {
-    role_name = aws_iam_role.prasanth_role.name
-  }
-
-  provisioner "local-exec" {
-    command     = "aws iam list-attached-role-policies --role-name ${aws_iam_role.prasanth_role.name} --query 'AttachedPolicies[].PolicyArn' --output text | xargs -I {} aws iam detach-role-policy --role-name ${aws_iam_role.prasanth_role.name} --policy-arn {}"
-    interpreter = ["bash", "-c"]
-  }
-}
-
-resource "null_resource" "remove_prasanth_role" {
-  depends_on = [null_resource.detach_policies]
-
-  triggers = {
-    role_name = aws_iam_role.prasanth_role.name
-  }
-
-  provisioner "local-exec" {
-    command     = "aws iam delete-role --role-name ${aws_iam_role.prasanth_role.name}"
-    interpreter = ["bash", "-c"]
-  }
+module "windows" {
+  source              = "/home/vishal/automation/terraform/lab07s1/modules/windows"
+  windows_rg_location = module.resource_group.windows_rg_location
+  windows_rg_name     = module.resource_group.windows_rg_name
+  subnet2_id          = module.network.subnet2_id
 }
